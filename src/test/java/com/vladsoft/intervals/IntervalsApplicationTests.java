0d9fc53eceff26e363d80db6ed8b98e40bc653f1
Vlad RADU <karamazovnew@yahoo.com>
2020-02-04 17:18:00 +0000

    Maven Project initial commit

---
+++ src/test/java/com/vladsoft/intervals/IntervalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vladsoft.intervals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntervalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
